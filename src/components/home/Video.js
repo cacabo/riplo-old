f9d7234269ec04987b35d64f828fb745cb67db34
ccabo1 <ccabo@seas.upenn.edu>
2018-03-16 01:42:37 -0400

    Adding strapper

---
+++ src/components/home/Video.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Video extends Component {
+  render() {
+    return (
+      <div>
+        https://s3.amazonaws.com/riplo/RiploVideo.mp4
+      </div>
+    );
+  }
+}
+
+export default Video;
