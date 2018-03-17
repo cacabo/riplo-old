e4ee4947fb7e1cb253bf8eba5417155f5d20cd56
Adam Ripley <aripley@wharton.upenn.edu>
2018-03-17 16:43:13 -0400

    frontend for contact

--- src/components/home/Contact.js
+++ src/components/home/Contact.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import autosize from 'autosize';
+import axios from 'axios';
 
-class Home extends Component {
+class Contact extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      first: '',
+      last: '',
+      subject: '',
+      body: '',
+      success: '',
+      error: '',
+    };
   }
 
   componentDidMount() {
@@ -13,7 +23,41 @@ class Home extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log("SUBMIT");
+    axios.post('/api/contact', {
+      first: this.state.first,
+      last: this.state.last,
+      subject: this.state.subject,
+      body: this.state.body
+    })
+    .then(resp => {
+      if (resp.data.success) {
+        this.setState({
+          first: '',
+          last: '',
+          subject: '',
+          body: '',
+          success: 'Message sent!',
+          error: '',
+        });
+      } else {
+        this.setState({
+          error: resp.data.error,
+          success: '',
+        })
+      }
+    })
+    .catch(err => {
+      this.setState({
+        error: 'Hmmm there was an error sending your message, reach us at hello@riplo.io',
+        success: '',
+      });
+    })
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
   }
 
   render() {
@@ -22,18 +66,18 @@ class Home extends Component {
         <h4>Contact us</h4>
         <div className="row">
           <div className="col-6">
-            <input className="form-control" placeholder="First" />
+            <input onChange={this.handleChange} name="first" className="form-control" placeholder="First" />
           </div>
           <div className="col-6">
-            <input className="form-control" placeholder="Last" />
+            <input onChange={this.handleChange} name="last" className="form-control" placeholder="Last" />
           </div>
         </div>
-        <input className="form-control" placeholder="Subject" />
-        <textarea className="form-control" placeholder="What are you looking for..." />
+        <input onChange={this.handleChange} name="subject" className="form-control" placeholder="Subject" />
+        <textarea onChange={this.handleChange} name="body" className="form-control" placeholder="What are you looking for..." />
         <input className="btn" type="submit" value="Send" />
       </form>
     );
   }
 }
 
-export default Home;
+export default Contact;
